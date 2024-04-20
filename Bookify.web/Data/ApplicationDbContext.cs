a832958dcb9ebc469561be069a04277b39836bb5
ganna <gannaosma@gmail.com>
2024-04-20 16:40:13 +0200

    Add Categories model

--- Bookify.web/Data/ApplicationDbContext.cs
+++ Bookify.web/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Bookify.web.Core.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bookify.web.Data
@@ -9,5 +10,7 @@ namespace Bookify.web.Data
             : base(options)
         {
         }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
